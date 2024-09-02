9ba2ac11f5c727970c6f91102ce5cb82e2887d7e
Harr-izle <harrietbuadee@gmail.com>
2024-09-02 15:21:25 +0000

    task one completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { interval, of, from, concat, Observable, throwError } from 'rxjs';
+import { take, catchError } from 'rxjs/operators';
+
+
 
 @Component({
   selector: 'app-root',
@@ -10,4 +14,49 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'RxJs-Observables';
+
+
+
+
+
+  constructor() {
+
+      // task One
+const numbers$ = of(100, 101, 102, 103, 104);
+numbers$.subscribe({
+  next: (value) => console.log('Emitted value:', value),
+  complete: () => console.log('Observable completed')
+});
+
+
+// task two
+
+// const favoriteColors = ['Red', 'Pink', 'Green', 'Yellow', 'Purple'];
+
+// const colors$ = from(favoriteColors);
+
+// colors$.subscribe({
+//   next: (value) => console.log('Emitted value:', value),
+//   complete: () => console.log('Observable completed')
+// });
+
+
+// task three
+// const interval$ = interval(1000).pipe(take(10));
+
+// interval$.subscribe({
+//   next: (value) => console.log('Emitted value:', value),
+//   complete: () => console.log('Observable completed')
+// });
+
+
+
+
+
+
+
+
+
+
+  }
 }
